dd79b46791296878cf5ebead6bed756c8031a479
Andrew Houts <ahouts4@gmail.com>
2022-10-30 12:12:56 -0500

    switch to ruby style function definitions, allow omission of trailing comma

--- src/lib.rs
+++ src/lib.rs
@@ -16,43 +16,42 @@ pub use compiler::Compiler;
 pub use vm::{Value, Vm};
 
 pub static PRELUDE: &str = r#"
-let nd =
-    fn (n, d,)
-        if n == 0 
-            then 0 
-            else __random d + nd(n - 1, d,);
+let nd = |n, d|
+    if n == 0 
+        then 0 
+        else __random d + nd(n - 1, d);
 
-let min = fn (a, b,) {
+let min = |a, b| {
     let av = a + 0;
     let bv = b + 0;
     if a < b then a else b
 };
-let max = fn (a, b,) {
+let max = |a, b| {
     let av = a + 0;
     let bv = b + 0;
     if a > b then a else b
 };
 
-let nd4 = fn (n,) nd(n,4,);
-let d4 = fn () nd4(1,);
+let nd4 = |n| nd(n,4);
+let d4 = || nd4(1);
 
-let nd6 = fn (n,) nd(n,6,);
-let d6 = fn () nd6(1,);
+let nd6 = |n| nd(n,6);
+let d6 = || nd6(1);
 
-let nd8 = fn (n,) nd(n,8,);
-let d8 = fn () nd8(1,);
+let nd8 = |n| nd(n,8);
+let d8 = || nd8(1);
 
-let nd10 = fn (n,) nd(n,10,);
-let d10 = fn () nd10(1,);
+let nd10 = |n| nd(n,10);
+let d10 = || nd10(1);
 
-let nd12 = fn (n,) nd(n,12,);
-let d12 = fn () nd12(1,);
+let nd12 = |n| nd(n,12);
+let d12 = || nd12(1);
 
-let nd20 = fn (n,) nd(n,20,);
-let d20 = fn () nd20(1,);
+let nd20 = |n| nd(n,20);
+let d20 = || nd20(1);
 
-let nd100 = fn (n,) nd(n,100,);
-let d100 = fn () nd100(1,);
+let nd100 = |n| nd(n,100);
+let d100 = || nd100(1);
 "#;
 
 #[cfg(test)]
@@ -105,9 +104,9 @@ mod tests {
     fn calls() {
         assert_result(
             r#"
-            let sub = fn (a, b,) a - b;
-            let div = fn (a, b,) a / b;
-            div(7, sub(5, 3,),)
+            let sub = |a, b| a - b;
+            let div = |a, b| a / b;
+            div(7, sub(5, 3))
         "#,
             Value::Number(3.5),
         );
@@ -148,8 +147,8 @@ mod tests {
 
     #[test]
     fn min_max() {
-        assert_result(r#"min(max(17, 13,), 23,)"#, Value::Number(17.));
-        assert_result(r#"min(d20, d20,)"#, Value::Number(18.));
+        assert_result(r#"min(max(17, 13), 23)"#, Value::Number(17.));
+        assert_result(r#"min(d20, d20)"#, Value::Number(18.));
     }
 
     fn assert_result(code: &str, value: Value) {
